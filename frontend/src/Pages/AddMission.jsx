280bed59aaf2809930e92a3af20eb28755f8a2a4
CKofficial-1797 <sanjaygorain1797@gmail.com>
2026-02-12 10:12:56 +0530

    feat: add application pages

---
+++ frontend/src/Pages/AddMission.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MissionForm from '../Component/addMission'
+
+const AddMission = () => {
+  return (
+    <>
+    <div className=' relative w-full bg-black py-20'>
+    <MissionForm/>
+    </div>
+    
+    </>
+  )
+}
+
+export default AddMission
---
+++ frontend/src/Pages/Signup.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SignupFormDemo } from '../Component/SignUp1'
+
+const Signup = () => {
+  return (
+    <>
+    <div className=' relative w-full py-20 bg-gradient-to-b from-black via-gray-900 to-black border-y-2 border-blue-900/50 shadow-[0_0_50px_theme(colors.blue.900/0.3) bg-opacity-70'>
+    <SignupFormDemo/></div>
+    </>
+  )
+}
+
+export default Signup
